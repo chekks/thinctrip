54a775240c4ea8adfb76354d721a0bb0ca3e5150
Cheska <cheska.portfolio2018@gmail.com>
2021-09-06 02:13:52 +0800

    Created admin tour packages page

--- client/src/admin/components/sidebar/SubMenu.js
+++ client/src/admin/components/sidebar/SubMenu.js
@@ -13,26 +13,27 @@ const SidebarLabel = styled.span`
 `;
 
 const DropdownLink = styled(Link)`
-	padding: 8px 0 8px 35px;
+	padding: 10px 15px 10px 35px;
 	display: flex;
 	align-items: center;
 	text-decoration: none;
 	color: #403F38;
-	font-size: 16px;
+	font-size: 14px;
 	transition: all .5s ease-in-out;
-	margin-left: 50px;
+	margin-left: 30px;
 `;
 
 const SubNav = styled.nav `
 	opacity: ${props => props.transform ? '1' : '0'};
-	transition: opacity 300ms ease-in-out;
+	transition: max-height 0.25s ease-in;
 	background: #F5F8F9;
+	max-height: ${props => props.transform ? '100%' : '0'};
 `;
 
-const HasSubNav = styled.div `
+const Arrow = styled.div `
 	svg {
 		position: absolute;
-		right: 15px;
+		right: 0;
 		top: ${props => props.transform ? '17px' : '13px'};
 		transform: ${props => props.transform ? 'rotateX(180deg)' : 'rotateX(0deg)'};
 	}
@@ -57,15 +58,14 @@ const SubMenu = ({item}) => {
 				<AdminBoxIcon icon={item.icon}/>
 				<SidebarLabel>{item.label}</SidebarLabel>
 
-				<HasSubNav transform={subnav}>{hasSubNav}</HasSubNav>
+				<Arrow transform={subnav}>{hasSubNav}</Arrow>
 			</Link>
 
 			<SubNav transform={subnav}>
 				{subnav && item.subMenu.map((item, index) => {
 					return (
-						<DropdownLink key={index} onClick={updateSetSubnav}>
-							<AdminBoxIcon icon={item.icon}/>
-							<SidebarLabel>{item.label}</SidebarLabel>
+						<DropdownLink key={index} to={item.link}>
+							{item.label}
 						</DropdownLink>
 					)
 				})}
