de0be4eebc393576b18a48a4a107a1decfdea209
judyannp <judyannpaulate@gmail.com>
2022-03-19 16:32:04 +0800

    Fix merge conflicts

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ const store = configureStore()
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App/>
+      <App />
     </Provider>
-  </BrowserRouter>, 
-document.getElementById('root'));
+  </BrowserRouter>,
+  document.getElementById('root'));
